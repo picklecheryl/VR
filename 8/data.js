6fb6b534cabd42f8681199edd1250515249e4816
picklecheryl <157569940+picklecheryl@users.noreply.github.com>
2024-03-27 14:51:04 +0800

    8

---
+++ 8/data.js
@@ -0,0 +1,291 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "岔路",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.01437037956153,
+          "pitch": 0.23473094336163847,
+          "rotation": 0.7853981633974483,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "八卦正",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9808343600217135,
+          "pitch": 0.3292497793288369,
+          "rotation": 5.497787143782138,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.5832939656761127,
+          "pitch": 0.4484782555026783,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "八卦本人",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.5662246971371374,
+        "pitch": 0.2933859692437828,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09496633172649283,
+          "pitch": 0.13074940369309118,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 1.9900593836955665,
+          "pitch": 0.37569811754024407,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.066671145923266,
+          "pitch": 0.24280218144313892,
+          "title": "Title",
+          "text": "不知道是幹嘛但看起來很像八卦陣"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "八卦正花園",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9590281298160956,
+          "pitch": 0.36706325666207107,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.6746657454829901,
+          "pitch": 0.20874457931332202,
+          "rotation": 5.497787143782138,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.09618812581485336,
+          "pitch": 0.12522630979062832,
+          "title": "小花園",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "露天演出台",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 1.5544520359121279,
+        "pitch": 0.18511907872724187,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3606207354461484,
+          "pitch": 0.038981919706614576,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -2.499803431700105,
+          "pitch": 0.11691283132709351,
+          "rotation": 0.7853981633974483,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5621180069614304,
+          "pitch": 0.047706943246696554,
+          "title": "露天演出台",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "別人的家",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 1.5454305562602624,
+        "pitch": 0.12827187610731627,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.547618356655521,
+          "pitch": 0.29711201183867963,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.2517338048145907,
+          "pitch": -0.005408072499735184,
+          "title": "冰箱",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "八卦陣",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
