0a7f9b3a2da12c0f11a156def45c14ed33b52bd8
picklecheryl <157569940+picklecheryl@users.noreply.github.com>
2024-03-27 14:42:47 +0800

    home\garden

---
+++ garden/data.js
@@ -0,0 +1,329 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -0.6074553862819023,
+        "pitch": 0.418115218659878,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.28110356616071286,
+          "pitch": 0.31296321586078335,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.11952980909262578,
+          "pitch": 0.2234717395929806,
+          "title": "售票亭",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "路",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -3.105124985336836,
+        "pitch": 0.12775841223608708,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1319896653293924,
+          "pitch": 0.16891458611935306,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.6580755093559887,
+          "pitch": 0.36173295981171094,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.05715465110030138,
+          "pitch": 0.09586465804816768,
+          "title": "入口",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.9741777068837614,
+          "pitch": -0.0028262500837676896,
+          "title": "Title",
+          "text": "繼續往前走！"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "岔路",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -3.0187004035138383,
+        "pitch": 0.2680940753434591,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0442836362750683,
+          "pitch": 0.401150022503014,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.3554123394139488,
+          "pitch": 0.25999106314891307,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.396910750676759,
+          "pitch": 0.16499390946108505,
+          "title": "花",
+          "text": "現在是花剛枯萎的時期<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "花園",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 1.5347388941442297,
+        "pitch": 0.36420327216467285,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0877095393529226,
+          "pitch": 0.29060423901762356,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 1.593228768381966,
+          "pitch": 0.1417702466910793,
+          "rotation": 4.71238898038469,
+          "target": "5-"
+        },
+        {
+          "yaw": -3.10578840271633,
+          "pitch": 0.12816237050752832,
+          "rotation": 0.7853981633974483,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.37078238968959454,
+          "pitch": 0.3861614968302547,
+          "title": "Title",
+          "text": "一些些花"
+        },
+        {
+          "yaw": 1.0061575553098123,
+          "pitch": 0.1337385370750912,
+          "title": "Title",
+          "text": "一些些花苞"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "房子",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8462450796285026,
+          "pitch": 0.1523848064544584,
+          "rotation": 7.853981633974483,
+          "target": "5-"
+        },
+        {
+          "yaw": -3.09470262417096,
+          "pitch": 0.0060062023323403935,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.36919596404721844,
+          "pitch": 0.07881265906105028,
+          "title": "Title",
+          "text": "房子<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "貧瘠",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3199401660882142,
+          "pitch": 0.28517018198116517,
+          "rotation": 1.5707963267948966,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.09246075316780633,
+          "pitch": 0.25953100664279205,
+          "rotation": 10.995574287564278,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.185189246704139,
+          "pitch": -0.4554924523844619,
+          "title": "枯枝",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
